19b5ef37f0efb0d4f425329067f522a8e46f1ba4
EmilsL <emils.lipsans@gmail.com>
2022-05-31 16:16:36 +0300

    Set virtual files as default

--- src/gui/wizard/owncloudadvancedsetuppage.cpp
+++ src/gui/wizard/owncloudadvancedsetuppage.cpp
@@ -83,6 +83,8 @@ OwncloudAdvancedSetupPage::OwncloudAdvancedSetupPage()
 
     _ui.rVirtualFileSync->setText(tr("Use &virtual files instead of downloading content immediately%1").arg(bestAvailableVfsMode() == Vfs::WindowsCfApi ? QString() : tr(" (experimental)")));
 
+    QTimer::singleShot(0, this, &OwncloudAdvancedSetupPage::slotVirtualFileSyncClicked);
+
     connect(this, &OwncloudAdvancedSetupPage::completeChanged, this, [this] {
         if (wizard() && owncloudWizard()->authType() == OCC::DetermineAuthTypeJob::AuthType::OAuth) {
             // For OAuth, disable the back button in the Page_AdvancedSetup because we don't want
