a57c1402736bd4c9778ef635ed1062548f6ca8ad
EmilsL <emils.lipsans@gmail.com>
2022-09-09 14:47:49 +0300

    Change autostart placement

--- src/gui/generalsettings.cpp
+++ src/gui/generalsettings.cpp
@@ -48,8 +48,7 @@ GeneralSettings::GeneralSettings(QWidget *parent)
     , _currentlyLoading(false)
 {
     _ui->setupUi(this);
-    _ui->crashreporterCheckBox->setVisible(false);
-    _ui->autostartCheckBox->setChecked(true);
+
     connect(_ui->desktopNotificationsCheckBox, &QAbstractButton::toggled,
         this, &GeneralSettings::slotToggleOptionalDesktopNotifications);
 #ifdef Q_OS_WIN
@@ -85,7 +84,8 @@ GeneralSettings::GeneralSettings(QWidget *parent)
     });
 
 #ifndef WITH_CRASHREPORTER
-    
+    _ui->crashreporterCheckBox->setVisible(false);
+    _ui->autostartCheckBox->setChecked(true);
 #endif
 
     // Hide on non-Windows, or WindowsVersion < 10.
